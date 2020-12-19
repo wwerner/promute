89943a0f1d6569f29f9db678eafa40b1064f9b2e
Wolfgang Werner <mail@wolfgang-werner.net>
2020-12-19 17:58:19 +0100

    Finish extension

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # promute
 Promute is a Chrome Extension that automatically mutes twitter accounts sending promotions to your timeline as you scroll by.
+
+It does the simplest thing that could possibly work.
+Within a scroll listener, it ...
+* ... identifies promoted tweets by the 'Promoted' label at the bottom,
+* ... opens the tweets context menu,
+* ... and clicks the mute button
+
--- promute.js
+++ promute.js
@@ -1,4 +1,5 @@
-var xPathEvaluator = $x
+const XPATH_PROMOTED_TWEET_MENU_ACTIVATORS = '//span[text()="Promoted"]/../../../..//div[@data-testid="caret"]'
+const XPATH_MUTE_BUTTON = '//*[@role="menuitem"]//span[starts-with(text(),"Mute")]'
 
 function debounce(func, wait = 100) {
     let timeout;
@@ -11,15 +12,26 @@ function debounce(func, wait = 100) {
 }
 
 mutePromoters = () => {
-    xPathEvaluator()
-        .forEach(promotedTweetMenuActivator => {
-            promotedTweetMenuActivator.click();
-            setTimeout(() => {
-                let muteButton = xPathEvaluator('//*[@role="menuitem"]//span[starts-with(text(),"Mute")]')[0];
-                console.log(muteButton.innerText);
-                muteButton.click()
-            }, 250)
-        })
+    let promotedTweetActivators = document.evaluate(XPATH_PROMOTED_TWEET_MENU_ACTIVATORS, document.body, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null)
+    let activators = []
+    if (promotedTweetActivators){
+        let promotedTweetActivator = promotedTweetActivators.iterateNext();
+        while(promotedTweetActivator) {
+            activators.push(promotedTweetActivator)
+            promotedTweetActivator = promotedTweetActivators.iterateNext();
+        }
+    }
+
+    activators.forEach(promotedTweetMenuActivator => {
+        promotedTweetMenuActivator.click();
+        setTimeout(() => {
+            let muteButton = document.evaluate(XPATH_MUTE_BUTTON, document.body, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+            if(muteButton) {
+                muteButton = muteButton.singleNodeValue
+            }
+            muteButton.click()
+        }, 250)
+    })
 }
 
 debouncedMutePromoters = debounce(mutePromoters, 1000)
