5b00d3cec633d35ee8235fc7c75bda6da6e42694
Wolfgang Werner <mail@wolfgang-werner.net>
2020-12-29 13:50:44 +0100

    Make xpaths language independent

--- promute.js
+++ promute.js
@@ -1,5 +1,5 @@
-const XPATH_PROMOTED_TWEET_MENU_ACTIVATORS = '//span[text()="Promoted"]/../../../..//div[@data-testid="caret"]'
-const XPATH_MUTE_BUTTON = '//*[@role="menuitem"]//span[starts-with(text(),"Mute")]'
+const XPATH_PROMOTED_TWEET_MENU_ACTIVATORS = '//div[@role="group"]/following-sibling::div/../..//div[@data-testid="caret"]'
+const XPATH_MUTE_BUTTON = '//div[@role="menu"]//div[@data-testid="block"]/preceding-sibling::div[@role="menuitem"][1]'
 
 function debounce(func, wait = 100) {
     let timeout;
