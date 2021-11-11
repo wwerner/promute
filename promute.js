e364647d2342b7e071fbd26c7edebb05ad00def1
Wolfgang Werner <wolfgang.werner@gmail.com>
2021-11-11 19:30:43 +0100

    Update XPath for promoted tweet detection

--- promute.js
+++ promute.js
@@ -1,4 +1,4 @@
-const XPATH_PROMOTED_TWEET_MENU_ACTIVATORS = '//div[@role="group"]/following-sibling::div/../..//div[@data-testid="caret"]'
+const XPATH_PROMOTED_TWEET_MENU_ACTIVATORS = '//div[@role="group"]/../following-sibling::div/../../..//div[@aria-label="More"]'
 const XPATH_MUTE_BUTTON = '//div[@role="menu"]//div[@data-testid="block"]/preceding-sibling::div[@role="menuitem"][1]'
 
 function debounce(func, wait = 100) {
